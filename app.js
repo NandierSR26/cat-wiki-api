41043cf5d415343204957bb344852ce151c043f3
Nandier Ruiz <nandiersruiz@outlook.com>
2022-06-10 23:19:54 -0500

    settings

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ const app = express();
 
 dbConnection();
 
+app.use( express.static('public') );
+
 app.use(cors())
+
+const whiteList = [ 'http://localhost:4200' ];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 app.use(express.json());
 
